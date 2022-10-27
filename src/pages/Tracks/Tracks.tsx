6159052e80bd27c2548a10e9c7ac0dfad5386d91
Miller Barahona <mbarahona@robinfood.com>
2022-10-26 23:55:02 -0500

    view tracks

--- src/pages/Tracks/Tracks.tsx
+++ src/pages/Tracks/Tracks.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { tracksStore, userStore } from "../../state";
 import { checkRefresh, getTracks, getUserLocal } from "../../utilities"
-import styles from '../Artists/Artist.module.css'
+import styles from './Tracks.module.css'
 
 function Tracks() {
   const userState = userStore()
@@ -23,14 +23,17 @@ function Tracks() {
       <div className={styles.container}>
         <h1>Top Tracks</h1>
         <div className={styles.listContainer}>
-          {
-            tracksState.tracks.map((track, index) => (
-              <div key={index} onClick={() => {navigate(`/tracks/${track.id}`)}} style={{cursor: 'pointer'}}>
-                <img src={track.album.images[0].url} className={styles.imgPoster} />
-                <p>{track.name} - {track.album.name}</p>
-              </div>
-            ))
-          }
+        {
+        tracksState.tracks.map((track, index) => (
+          <div key={index} className={styles.trackItem} onClick={ () => {navigate(`/tracks/${track.id}`)}}>
+            <img src={track.album.images[2].url} alt="" />
+            <div>
+              <h3>{track.name}</h3>
+              <h4>{track.artists[0].name} . {track.album.name}</h4>
+            </div>
+          </div>
+        ))
+      }
         </div>
       </div>
     </div>
